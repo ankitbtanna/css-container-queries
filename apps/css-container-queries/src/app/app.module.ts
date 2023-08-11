fb7b72bc9e21e702d0b11dcb3e1556d8ccc2fc78
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-08-11 15:40:34 +1000

    initial commit

--- apps/css-container-queries/src/app/app.module.ts
+++ apps/css-container-queries/src/app/app.module.ts
@@ -4,9 +4,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    CardComponent,
+    CardComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
---
+++ apps/css-container-queries/src/app/card/card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'css-container-queries-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent {}
