bc8f6c6cadf054504a4dde81fbc7fc695b4eff83
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-08-11 10:00:24 +1000

    Initial commit

---
+++ apps/css-container-queries/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'css-container-queries-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'css-container-queries';
+}
